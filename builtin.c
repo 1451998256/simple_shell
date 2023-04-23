024392128ba5d7fee4b023b4024335ccf836ea51
1451998256 <122821018+1451998256@users.noreply.github.com>
2023-04-23 11:07:46 +0200

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -35,7 +35,6 @@ int _myexit(info_t *info)
  * _mycd - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
- *
  * Return: Always 0
  */
 int _mycd(info_t *info)
@@ -46,31 +45,12 @@ int _mycd(info_t *info)
 	s = getcwd(buffer, 1024);
 	if (!s)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
-
 	if (!info->argv[1])
-	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-			chdir_ret = /* TODO: what should this be? */
-				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
-		else
-			chdir_ret = chdir(dir);
-	}
+		return _mycd_no_args(info, s);
 	else if (_strcmp(info->argv[1], "-") == 0)
-	{
-		if (!_getenv(info, "OLDPWD="))
-		{
-			_puts(s);
-			_putchar('\n');
-			return (1);
-		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = /* TODO: what should this be? */
-				chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
-	}
+		return _mycd_oldpwd(info, s);
 	else
 		chdir_ret = chdir(info->argv[1]);
-
 	if (chdir_ret == -1)
 	{
 		print_error(info, "can't cd to ");
@@ -83,7 +63,7 @@ int _mycd(info_t *info)
 	}
 	return (0);
 }
-
+	
 /**
  * _myhelp - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
